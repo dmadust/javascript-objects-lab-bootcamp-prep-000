439d2844d4b3fd9c351dff3c2177144491aeb198
dmadust@gmail.com <dmadust@gmail.com>
2018-04-20 23:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object, {[key] : value});
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) { 
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
